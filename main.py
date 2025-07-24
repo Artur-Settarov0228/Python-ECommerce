3c324775245cc843303f12bb3ea2b0edd367e871
settarovartur <settarovartur2@gamil.com>
2025-07-24 23:30:05 +0500

    bajarildi

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+import json
+from sys import exit
+from models.product import Product
 from models.user import User
+from utils.printers import print_table ,  print_status
 
+while True:
+    print_table()
+    
+    tanlov = int(input("tanlang : "))
+    if tanlov == 1:
+        User.create_user()
 
-User.create_user()
+    elif tanlov == 2:
+        products = Product.load_products()
